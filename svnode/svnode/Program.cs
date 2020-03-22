6b9685550d18df9988ba0f81f0267ecb44b0363b
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2020-03-22 11:48:53 +0700

    s

--- svnode/svnode/Program.cs
+++ svnode/svnode/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
 using System.Text;
 
 namespace svnode
@@ -8,6 +11,26 @@ namespace svnode
     {
         static void Main(string[] args)
         {
+            string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string platform = "64";
+
+            string file_node = Path.Combine(path, "x" + platform + @"\node.exe");
+
+            if (File.Exists(file_node))
+            {
+                Process p = new Process();
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.RedirectStandardError = true;
+                p.StartInfo.RedirectStandardInput = true;
+                p.StartInfo.FileName = file_node;
+                string argument = @" --max-old-space-size=4096 app.js";
+                p.StartInfo.Arguments = argument;
+                p.Start();
+                //p.WaitForExit();
+                Console.ReadLine();
+            }
+
         }
     }
 }
--- svnode/test/app.js
+++ svnode/test/app.js
@@ -1,1 +1,40 @@
-﻿
+﻿//let PORT = 8080;
+
+const HTTP = require('http');
+const SERVER = HTTP.createServer().listen(8080, '127.0.0.1');
+//PORT = SERVER.address().port;
+//console.log(PORT);
+
+SERVER.on('request', function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html; charset: UTF-8" });
+    res.write("Hello from Node! ");
+    res.write(" Server listening on port " + this.address().port + ' at ' + new Date().toLocaleString());
+    res.end();
+});
+
+////#region [ READLINE ]
+
+//const READ_LINE = require("readline");
+//const RL = READ_LINE.createInterface({ input: process.stdin, output: process.stdout });
+//RL.on("line", function (line) {
+//    let text = line.trim();
+//    const a = text.split(' ');
+//    let cmd = a[0].toLowerCase();
+//    switch (cmd) {
+//        case 'exit':
+//            process.exit();
+//            break;
+//        case 'cls':
+//        case 'clear':
+//            console.clear();
+//            break;
+//        case 'info':
+//            console.clear();
+//            break;
+//        default:
+//            console.clear();
+//            break;
+//    }
+//});
+
+////#endregion
